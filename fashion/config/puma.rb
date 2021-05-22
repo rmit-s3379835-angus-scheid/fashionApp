78117a034a781bdbdf27b76669ea13da938e7282
Ubuntu <ubuntu@ip-172-31-37-171.ec2.internal>
2021-05-22 10:26:28 +0000

    modified production and puma

--- fashion/config/puma.rb
+++ fashion/config/puma.rb
@@ -41,3 +41,6 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
+
+
+ActiveRecord::Base.establish_connection if defined?(ActiveRecord)
